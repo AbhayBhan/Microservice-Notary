0000d771888990615defd2d88cdecbdcdc8c27ff
AbhayBhan <abhaybhan197@gmail.com>
2023-07-01 08:00:06 +0530

    Init Commit-1

--- src/subscription/subscription.controller.js
+++ src/subscription/subscription.controller.js
@@ -7,11 +7,6 @@ export class SubscriptionController{
         this.subscriptionService = new SubscriptionService();
     }
 
-    @Post('/create')
-    createSub(@Body() reqbody){
-        return this.subscriptionService.createSubscription(reqbody);
-    }
-
     @Post('/webhook')
     webhookEvents(@Request() reqbody){
         return this.subscriptionService.webhookEvents(reqbody);
--- src/subscription/subscription.service.js
+++ src/subscription/subscription.service.js
@@ -8,43 +8,6 @@ export class SubscriptionService {
     this.endpointSecret = "whsec_ce4581159005ef971fbc95e7bd44523794033bcac5b9600d72cc34d73fd888be";
   }
 
-  async createSubscription(reqbody) {
-    // create a stripe customer
-    const customer = await this.stripe.customers.create({
-      name: reqbody.name,
-      email: reqbody.email,
-      payment_method: reqbody.paymentMethod,
-      invoice_settings: {
-        default_payment_method: reqbody.paymentMethod,
-      },
-    });
-
-    // get the price id from the front-end
-    const priceId = reqbody.priceId;
-
-    // create a stripe subscription
-    const subscription = await this.stripe.subscriptions.create({
-      customer: customer.id,
-      items: [{ price: priceId }],
-      payment_settings: {
-        payment_method_options: {
-          card: {
-            request_three_d_secure: 'any',
-          },
-        },
-        payment_method_types: ['card'],
-        save_default_payment_method: 'on_subscription',
-      },
-      expand: ['latest_invoice.payment_intent'],
-    });
-
-    // return the client secret and subscription id
-    return {
-      clientSecret: subscription.latest_invoice.payment_intent.client_secret,
-      subscriptionId: subscription.id,
-    };
-  }
-
   async webhookEvents(reqbody) {
     let event = reqbody.body;
     switch (event.type) {
