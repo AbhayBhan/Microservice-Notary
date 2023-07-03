3b78a3c99ee7b28eb8d2240b2954bb4eaedf1e98
AbhayBhan <abhaybhan197@gmail.com>
2023-07-03 13:36:57 +0530

    Fixed Pino

--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import mongoose from 'mongoose';
-import { Logger } from 'nestjs-pino';
-
-// Activate this to Log Requests in console.
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule, {bufferLogs : true});
-//   app.useLogger(app.get(Logger));
-//   await app.listen(process.env.NODE_PORT);
-// }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
